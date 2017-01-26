0bd8d24f81d0dd00e9719149c8ad99a5535ebd49
Robert <pa8rut@yahoo.com>
2017-01-26 18:11:50 -0500

    added procedures

--- Procedures.sql
+++ Procedures.sql
@@ -1,9 +1,30 @@
+--insert user info into regstration
 create or replace procedure add_new_user
-(userEmail in varchar2, firstname in varchar2, middleName in varchar2,
-lastName in varchar2, phoneNumber in number, passwd in varchar2)
+(uEmail in varchar2, firstname in varchar2, middleName in varchar2,
+lastName in varchar2, phoneNumber in number, passwd in varchar2, outval out varchar2)
 is
 begin
-insert into userRegistration values (userEmail, firstName, middleName, lastName,
+if not exists (select * from userRegistration where userEmail = uEmail) then
+insert into userRegistration values (uEmail, firstName, middleName, lastName,
 phoneNumber, passwd);
+else
+outval := false;
+end if;
 end;
 /
+
+
+--checks for duplicate previous passwords
+create or replace procedure passwd_checker
+(uEmail in varchar2, passwrd in varchar, outval out varchar2)
+is
+a varchar2;
+begin
+if not exists (select * from passwordHistory where pastPass = passwrd) then
+insert into userRegistration (passwd) values (passwrd);
+else
+outval := false;
+end if;
+end;
+/
+
