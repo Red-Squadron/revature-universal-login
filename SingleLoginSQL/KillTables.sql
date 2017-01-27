8247560ba68c6e84557dd3b916f474326813340b
Robert <pa8rut@yahoo.com>
2017-01-27 12:07:50 -0500

    updating procedures

---
+++ SingleLoginSQL/KillTables.sql
@@ -0,0 +1,8 @@
+Drop table passwordHistory;
+drop table userRegistration;
+drop table SalesForceMock;
+
+drop procedure add_new_user;
+drop procedure passwd_checker;
+drop procedure update_phone;
+drop procedure validate_login;
