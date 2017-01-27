cab5f066e3db6ed69544c07f21ae755a9e65325e
David Gilbert <gilbe200@gmail.com>
2017-01-27 12:22:39 -0500

    update userDao with updatePhone and checkPassword methods

--- DAO/dao/UserDAO.java
+++ DAO/dao/UserDAO.java
@@ -33,13 +33,14 @@ public class UserDAO {
 
 	public boolean validateLogin(String userName, String password) {
 		try {
-			CallableStatement checkLogin = conn.prepareCall("{call validate_login(?,?)}");
+			CallableStatement checkLogin = conn.prepareCall("{call validate_login(?,?,?)}");
 
 			checkLogin.setString(1, userName);
 			checkLogin.setString(2, password);
+			checkLogin.registerOutParameter(3, Types.INTEGER);
 			checkLogin.executeUpdate();
 
-			if (checkLogin.getInt(1) == 1) {
+			if (checkLogin.getInt(3) == 1) {
 				return true;
 			} else {
 				return false;
@@ -52,7 +53,7 @@ public class UserDAO {
 
 	public boolean registerUser(String email, String fname, String mname, String lname, int phone, String pass) {
 		try{
-			CallableStatement registerUser = conn.prepareCall("{call add_new_user(?, ?, ?, ?, ?, ?)}");
+			CallableStatement registerUser = conn.prepareCall("{call add_new_user(?, ?, ?, ?, ?, ?, ?)}");
 			
 			registerUser.setString(1, email);
 			registerUser.setString(2, fname);
@@ -60,8 +61,10 @@ public class UserDAO {
 			registerUser.setString(4, lname);
 			registerUser.setInt(5, phone);
 			registerUser.setString(6, pass);
+			registerUser.registerOutParameter(7, Types.INTEGER);
+			registerUser.executeUpdate();
 			
-			if (registerUser.getInt(1) == 1) {
+			if (registerUser.getInt(7) == 1) {
 				return true;
 			} else {
 				return false;
@@ -71,5 +74,44 @@ public class UserDAO {
 		}
 		return false;
 	}
+	
+	public boolean checkPassword(String userName, String password) {
+		try {
+			CallableStatement checkPassword = conn.prepareCall("{call paswd_checker(?,?,?)}");
 
+			checkPassword.setString(1, userName);
+			checkPassword.setString(2, password);
+			checkPassword.registerOutParameter(3, Types.INTEGER);
+			checkPassword.executeUpdate();
+
+			if (checkPassword.getInt(3) == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean updatePhone(String email, int number) {
+		try {
+			CallableStatement updatePhone = conn.prepareCall("{call update_phone(?,?,?)}");
+
+			updatePhone.setString(1, email);
+			updatePhone.setInt(2, number);
+			updatePhone.registerOutParameter(3, Types.INTEGER);
+			updatePhone.executeUpdate();
+
+			if (updatePhone.getInt(3) == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
