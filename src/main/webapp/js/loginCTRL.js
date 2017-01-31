91b61784afd8235f2e6e359cf9e04380427b5bcb
David Gilbert <gilbe200@gmail.com>
2017-01-31 13:03:32 -0500

    Change to loginCTRL and RULServlet

--- src/main/webapp/js/loginCTRL.js
+++ src/main/webapp/js/loginCTRL.js
@@ -6,12 +6,16 @@
 module
 .controller('loginCtrl', function($scope, $http) {
 	$scope.validateLogin = function(userName, passPhrase) {
-		var validateString = {};
-		validateString.userName = userName;
-		validateString.password = passPhrase;
+		var validateString = "";
+		validateString += "userName=" + userName + "&password=" + passPhrase;
 		
-		// If the email address does not contain '@' then nothing will happen
-		$http.post("RULServlet/login", validateString).success(function(userInfo) {
+		
+		$http({
+	        method: 'POST',
+	        url: 'RULServlet/login',
+	        data: validateString,
+	        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	    }).success(function(userInfo) {
 			if(userInfo.valid === "true") {
 				$scope.user.authLvl = userInfo.authLvl;
 				$scope.user.userName = userInfo.userName;
