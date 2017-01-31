1e7dea75ad1bf4cdc8d6fe5b409b5d3741d87ea7
Nicholas <nickpaqu1@yahoo.com>
2017-01-31 12:01:10 -0500

    Added password validation to register CTRL

--- src/main/webapp/js/registerCTRL.js
+++ src/main/webapp/js/registerCTRL.js
@@ -4,19 +4,36 @@
  */
 module
 .controller('registerCtrl', function($scope, $http) {
-	$scope.registerUser = function(userName, passPhrase, firstName, lastName, middleName) {
+	$scope.registerUser = function(userName, firstName, lastName, middleName, passPhrase, passPhraseConfirm) {
 		newUser = {};
 		newUser.userName = userName;
 		newUser.passphrase = passPhrase;
 		newUser.firstName = firstName;
 		newUser.lastName = lastName;
 		newUser.lastName = middleName;
+		newUser.passPhraseConfirm = passPhraseConfirm;
 		
-		$http.post("RULServlet/register", newUser).success(function(isRegistered) {
-			if(isRegistered === "true") {
-				Angular.element("modalClose").triggerHandler("click");
-			}
-		})
+		if(newUser.passphrase !== newUser.passPhraseConfirm){
+			
+		} else if(newUser.passphrase.length < 8){
+			
+		} else if(newUser.passphrase.length > 75){
+			
+		} else if(newUser.passphrase === newUser.passphrase.toLowerCase()){
+			
+		} else if(newUser.passphrase === newUser.passphrase.toUpperCase()){
+			
+		} else if(!containsNumber(newUser.passphrase)){
+			
+		} else if(!containsSpecial(newUser.passphrase)){
+			
+		} else {
+			$http.post("RULServlet/register", newUser).success(function(isRegistered) {
+				if(isRegistered === "true") {
+					Angular.element("modalClose").triggerHandler("click");
+				}
+			})
+		}
 		
 		$scope.hovering = false;
 		
@@ -28,3 +45,95 @@ module
 		}
 	};
 });
+
+//Checks if the passed string contains a number
+var containsNumber = function(str){
+	if(str.includes('0')){
+		return true;
+	} else if(str.includes('1')){
+		return true;
+	} else if(str.includes('2')){
+		return true;
+	} else if(str.includes('3')){
+		return true;
+	} else if(str.includes('4')){
+		return true;
+	} else if(str.includes('5')){
+		return true;
+	} else if(str.includes('6')){
+		return true;
+	} else if(str.includes('7')){
+		return true;
+	} else if(str.includes('8')){
+		return true;
+	} else if(str.includes('9')){
+		return true;
+	} else {
+		return false;
+	}
+};
+
+// Checks if the passed string contains a special character
+var containsSpecial = function(str){
+	if(str.includes('-')){
+		return true;
+	} else if(str.includes('=')){
+		return true;
+	} else if(str.includes('[')){
+		return true;
+	} else if(str.includes(']')){
+		return true;
+	} else if(str.includes('\\')){
+		return true;
+	} else if(str.includes(';')){
+		return true;
+	} else if(str.includes(',')){
+		return true;
+	} else if(str.includes('.')){
+		return true;
+	} else if(str.includes('/')){
+		return true;
+	} else if(str.includes('~')){
+		return true;
+	} else if(str.includes('!')){
+		return true;
+	} else if(str.includes('@')){
+		return true;
+	} else if(str.includes('#')){
+		return true;
+	} else if(str.includes('$')){
+		return true;
+	} else if(str.includes('%')){
+		return true;
+	} else if(str.includes('^')){
+		return true;
+	} else if(str.includes('&')){
+		return true;
+	} else if(str.includes('*')){
+		return true;
+	} else if(str.includes('(')){
+		return true;
+	} else if(str.includes(')')){
+		return true;
+	} else if(str.includes('_')){
+		return true;
+	} else if(str.includes('+')){
+		return true;
+	} else if(str.includes('{')){
+		return true;
+	} else if(str.includes('}')){
+		return true;
+	} else if(str.includes('|')){
+		return true;
+	} else if(str.includes(':')){
+		return true;
+	} else if(str.includes('<')){
+		return true;
+	} else if(str.includes('>')){
+		return true;
+	} else if(str.includes('?')){
+		return true;
+	} else {
+		return false;
+	}
+};
