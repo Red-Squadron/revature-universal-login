ac0ddc880b11ba801c7b3b7a33a32ddbaa94aab3
John Radcliffe <johnjradcliffe@gmail.com>
2017-02-04 23:41:51 -0500

    Updating routing

--- src/main/webapp/route.js
+++ src/main/webapp/route.js
@@ -5,30 +5,28 @@ app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl : "login.html",
-
-
+            controller : "loginCtrl"
         })
         .when("/register", {
             templateUrl : "register.html",
-
-
+            controller : "registerCtrl"
         })
-          .when("/profiles", {
+        .when("/profiles", {
             templateUrl : "changeInfo.html",
-          })
-        .when("/welcome",
-        {
+            controller : "changeInformationCtrl"
+        })
+        .when("/welcome", {
         	 templateUrl : "welcome.html",
+           controller : "welcomeCtrl"
         })
-        .when("/updatepassword",
-        {
+        .when("/updatepassword", {
         	 templateUrl : "updatepassword.html",
+           controller : "updatePasswordCtrl"
         })
-        .when("/updatephone",
-        {
+        .when("/updatephone", {
         	 templateUrl : "updatephone.html",
+           controller : "updatePhoneCtrl"
         })
-
         .otherwise({
             redirectTo : "/login"
         });
