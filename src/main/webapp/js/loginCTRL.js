dc6a722a1b424a457e03b2e31a37e9c574a6397a
Jose Suquilanda <suqujl93@gmail.com>
2017-02-07 13:18:54 -0500

    Add stored js

--- src/main/webapp/js/loginCTRL.js
+++ src/main/webapp/js/loginCTRL.js
@@ -27,11 +27,10 @@ app
                     $scope.user.authTkn = userInfo.authTkn;
                     $scope.loginSuccess = "Successfully logged in!";
                     $scope.changeView('welcome');
-                    //var b =document.getElementById("click").click();
-
-                    //setHtmlStorage('authoLevel',userInfo.authLvl)
-    				        //setHtmlStorage('authToken',userInfo.authoTkn)
-
+                    
+                    setHtmlStorage('authoLevel',userInfo.authLvl)
+    				setHtmlStorage('authToken',userInfo.authoTkn)
+    				  
                     //TODO cookie implementation instead of in scope
                 } else
                     $scope.loginSuccess = "Wrong email/password!";
