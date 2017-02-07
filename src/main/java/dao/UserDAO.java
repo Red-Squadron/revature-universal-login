52e7ed57e97a1e3d371e8157b64eb2e7650d645b
Joe <joseph.caruso@mymail.champlain.edu>
2017-02-07 17:15:33 -0500

    ???

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -217,9 +217,10 @@ public class UserDAO {
 	 * @throws SQLException 
 	 */
 	public boolean updatePhone(String email, String number) throws SQLException {
-		CallableStatement updatePhone = conn.prepareCall("{call update_phone(?,?,?)}");
+		CallableStatement updatePhone = null;
 		try {
-
+			updatePhone = conn.prepareCall("{call update_phone(?,?,?)}");
+			
 			updatePhone.setString(1, email);
 			updatePhone.setString(2, number);
 			updatePhone.registerOutParameter(3, Types.INTEGER);
@@ -235,6 +236,8 @@ public class UserDAO {
 		} catch (SQLException e) {
 			updatePhone.close();
 			logger.log(Level.SEVERE, e.getMessage(),e);
+		}catch(NullPointerException e){
+			logger.log(Level.SEVERE, "I have no idea how you did this....",e);
 		}
 		return false;
 	}
