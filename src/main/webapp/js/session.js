eac884a573566e18f74f5824fd9d8d2cbc8d4cea
Joe <joseph.caruso@mymail.champlain.edu>
2017-02-07 13:42:23 -0500

    OMG

--- src/main/webapp/js/session.js
+++ src/main/webapp/js/session.js
@@ -6,7 +6,7 @@ function removeHtmlStorage(name) {
 
 function setHtmlStorage(name, value, expires) {
 
-    if (expires===undefined || expires==='null') { var expires = 3600; } // default: 1h if Not Defined
+    if (expires===undefined || expires===null) { expires = 3600; } // default: 1h if Not Defined
 
     var date = new Date();
     var schedule = Math.round((date.setSeconds(date.getSeconds()+expires))/1000);
@@ -22,7 +22,7 @@ function statusHtmlStorage(name) {
 
     // Get Schedule
     var stored_time = localStorage.getItem(name+'_time');
-    if (stored_time===undefined || stored_time==='null') { var stored_time = 0; }
+    if (stored_time===undefined || stored_time===null) { stored_time = 0; }
 
     // Expired
     if (stored_time < current) {
@@ -37,25 +37,3 @@ function statusHtmlStorage(name) {
         return 1;
     }
 }
-/*
-// Status
-var cache_status = statusHtmlStorage('cache_name');
-
-// Has Data
-if (cache_status == 1) {
-
-    // Get Cache
-    var data = localStorage.getItem('cache_name');
-    alert(data);
-
-// Expired or Empty Cache
-} else {
-
-    // Get Data
-    var data = 'Pay in cash :)';
-    alert(data);
-
-    // Set Cache (30 seconds)
-    if (cache) { setHtmlStorage('cache_name', data, 30); }
-
-}*/
