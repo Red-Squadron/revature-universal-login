30eb28f682a55dc6102216c2322250d67d0b3781
Zahidul Islam <islamz1214@gmail.com>
2017-02-07 22:24:26 -0500

    Improve access route

--- src/main/webapp/route.js
+++ src/main/webapp/route.js
@@ -5,44 +5,51 @@ app.config(function ($routeProvider) {
     $routeProvider
         .when("/", {
             templateUrl : "login.html",
-            controller : "loginCtrl"
+            controller : "loginCtrl",
+            requireLogin: false
         })
         .when("/register", {
             templateUrl : "register.html",
-            controller : "registerCtrl"
+            controller : "registerCtrl",
+            requireLogin: false
         })
         .when("/profiles", {
             templateUrl : "changeInfo.html",
-            controller : "changeInformationCtrl"
+            controller : "changeInformationCtrl",
+            requireLogin: true
         })
         .when("/welcome", {
         	 templateUrl : "welcome.html",
-           controller : "welcomeCtrl"
+           controller : "welcomeCtrl",
+           requireLogin: true
         })
         .when("/updatepassword", {
         	 templateUrl : "updatepassword.html",
-           controller : "updatePasswordCtrl"
+           controller : "updatePasswordCtrl",
+           requireLogin: true
         })
         .when("/updatephone", {
         	 templateUrl : "updatephone.html",
-           controller : "updatePhoneCtrl"
+           controller : "updatePhoneCtrl",
+           requireLogin: true
         })
         .otherwise({
         	templateUrl : "404.html",
+            requireLogin: false
         });
 
 });
 
 app.run(function($rootScope, $location) {
   $rootScope.$on("$routeChangeStart", function(event, next) {
-
+    // If user is not logged in
     if (localStorage.getItem('authoLevel') == null) {
-     
-      if ( next.templateUrl === "register.html") {
+      // Check if user can access page without logging in
+      if (next.requireLogin === false) {
       } else {
+        // User does not have access, so redirect to login
         $location.path("/");
       }
     }
-
   });
 });
